a97f93eaea75f301c47eff561aab9c9d2e96fd5d
Nicolai <cwX3MfXK9YrCCc8eZk@gmail.com>
2024-03-17 00:13:37 +0100

    Bump version to {   '@vr-web-shop/auth': '1.0.0',   npm: '9.8.0',   node: '18.16.1',   acorn: '8.8.2',   ada: '1.0.4',   ares: '1.19.1',   brotli: '1.0.9',   cldr: '42.0',   icu: '72.1',   llhttp: '6.0.11',   modules: '108',   napi: '8',   nghttp2: '1.52.0',   nghttp3: '0.7.0',   ngtcp2: '0.8.1',   openssl: '3.0.9+quic',   simdutf: '3.2.2',   tz: '2022g',   undici: '5.21.0',   unicode: '15.0',   uv: '1.44.2',   uvwasi: '0.0.15',   v8: '10.2.154.26-node.26',   zlib: '1.2.13' }

--- contract_deploy.sh
+++ contract_deploy.sh
@@ -1,13 +1,4 @@
 
-# Check if the contract release branch exists
-if [ -z "$(git branch --list contract-release)" ]; then
-  # Create the contract release branch
-  git checkout -b contract-release
-else
-    # Checkout the contract release branch
-    git checkout contract-release
-fi
-
 # Bumb version
 npm version patch
 
@@ -16,16 +7,9 @@ git add .
 git commit -m "Bump version to $(npm version)"
 
 # Push changes
-git push origin contract-release
-
-# Checkout the main branch
-git checkout main
-
-# Merge the contract release branch
-git merge contract-release
-
-# Push changes
 git push origin main
+
+
 
 # Exit with success
 exit 0
