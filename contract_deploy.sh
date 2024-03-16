27bad76b456e7158423a1d53f40528a098683be2
Nicolai <cwX3MfXK9YrCCc8eZk@gmail.com>
2024-03-17 00:17:26 +0100

    Prepare deployment

--- contract_deploy.sh
+++ contract_deploy.sh
@@ -1,6 +1,8 @@
+git add .
+git commit -m "Prepare deployment"
 
 # Bumb version
-npm version patch
+npm run version patch
 
 # Commit changes
 git add .
@@ -9,7 +11,22 @@ git commit -m "Bump version"
 # Push changes
 git push origin main
 
+# Check if the contract release branch exists
+if [ -z "$(git branch --list contract-release)" ]; then
+  # Create the contract release branch
+  git checkout -b contract-release
+else
+    # Checkout the contract release branch
+    git checkout contract-release
+    # Merge the main branch
+    git merge main
+fi
 
+# Push changes
+git push origin contract-release
+
+# Checkout the main branch
+git checkout main
 
 # Exit with success
 exit 0
