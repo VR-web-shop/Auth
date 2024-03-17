648478ecaa2e63fd2894a37471e7d2bedb7f6b80
Nicolai <cwX3MfXK9YrCCc8eZk@gmail.com>
2024-03-17 02:31:39 +0100

    Prepare deployment

--- sdk_deploy.sh
+++ sdk_deploy.sh
@@ -2,14 +2,17 @@ echo "Commiting any changes made before bumping the version"
 git add .
 git commit -m "Prepare deployment"
 
+echo ""
 echo "Bumping the version"
 npm version patch
 
+echo ""
 echo "Pushing the changes to the main branch"
 git add .
 git commit -m "Bump version"
 git push origin main
 
+echo ""
 echo "Looking for the release branch"
 if [ -z "$(git branch --list sdk-release)" ]; then
   echo "Creating the release branch locally"
@@ -22,12 +25,15 @@ else
     git merge main
 fi
 
+echo ""
 echo "Pushing the release branch to the remote"
 git push origin sdk-release
 
+echo ""
 echo "Checking out the main branch"
 git checkout main
 
-echo "Deployment completed successfully"
+echo ""
+echo -e "\e[32mDeployment completed successfully\e[0m"
 echo "Find the triggered workflow at: https://github.com/VR-web-shop/Auth/actions"
 exit 0
