262e1d75d13f0c61b0f0b41aa8f734ad699ce1e0
Nicolai <cwX3MfXK9YrCCc8eZk@gmail.com>
2024-04-23 04:43:59 +0200

    update

--- db/models/index.cjs
+++ db/models/index.cjs
@@ -11,15 +11,9 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], {
-    ...config,
-    quoteIdentifiers: true,
-  });
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, {
-    ...config,
-    quoteIdentifiers: true,
-  });
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
